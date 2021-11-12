4d27f185ec0567c0878fb3814ed3ac7321973888
bealean <gbealean@gmail.com>
2021-11-12 10:07:40 -0500

    Controller JUnit Documentation Updates

--- java/tenmo-server/src/test/java/com/techelevator/tenmo/controller/ApplicationControllerTest.java
+++ java/tenmo-server/src/test/java/com/techelevator/tenmo/controller/ApplicationControllerTest.java
@@ -66,10 +66,11 @@ public class ApplicationControllerTest {
         UserDTO fromUser = new UserDTO(1001L, "john");
         UserDTO toUser = new UserDTO(1002L, "jane");
 
-        Transfer transfer = new Transfer(3001L,"Send","Approved",fromUser,toUser, new BigDecimal("50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", fromUser, toUser, new BigDecimal("50.00"));
 
         String transferContent = mapper.writeValueAsString(transfer);
-        mockMvc.perform(post("/transfers").contentType(MediaType.APPLICATION_JSON).content(transferContent)).andExpect(status().isUnauthorized());
+        mockMvc.perform(post("/transfers").contentType(MediaType.APPLICATION_JSON)
+                .content(transferContent)).andExpect(status().isUnauthorized());
     }
 
     @WithMockUser(username = "jane")
@@ -77,11 +78,11 @@ public class ApplicationControllerTest {
     public void getBalanceReturnsStatusOkAndUserBalance() throws Exception {
         UserDTO user = new UserDTO(1001L, "jane");
         userRepository.save(user);
-        Accounts account = new Accounts(2001,1001,new BigDecimal("1000.00"));
+        Accounts account = new Accounts(2001, 1001, new BigDecimal("1000.00"));
         accountsRepository.save(account);
         UserDTO otherUser = new UserDTO(1002L, "jamie");
         userRepository.save(otherUser);
-        Accounts otherAccount = new Accounts(2002,1002,new BigDecimal("2000.00"));
+        Accounts otherAccount = new Accounts(2002, 1002, new BigDecimal("2000.00"));
         accountsRepository.save(otherAccount);
 
         mockMvc.perform(get("/balance"))
@@ -92,7 +93,7 @@ public class ApplicationControllerTest {
 
     @WithMockUser(username = "MockUser1")
     @Test
-    public void getUsersReturnsStatusOkAndAllUsers() throws Exception {
+    public void getUsersForAuthenticatedUserReturnsStatusOkAndUserDTOList() throws Exception {
         UserDTO user1 = new UserDTO(1001L, "john");
         userRepository.save(user1);
         UserDTO user2 = new UserDTO(1002L, "jane");
@@ -110,14 +111,14 @@ public class ApplicationControllerTest {
     public void postTransferFromUserReturnsStatusCreatedAndTransfer() throws Exception {
         UserDTO userFrom = new UserDTO(1002L, "jane");
         userRepository.save(userFrom);
-        Accounts accountFrom = new Accounts(2002,1002,new BigDecimal("1000.00"));
+        Accounts accountFrom = new Accounts(2002, 1002, new BigDecimal("1000.00"));
         accountsRepository.save(accountFrom);
         UserDTO userTo = new UserDTO(1001L, "john");
         userRepository.save(userTo);
-        Accounts accountTo = new Accounts(2001,1001,new BigDecimal("1000.00"));
+        Accounts accountTo = new Accounts(2001, 1001, new BigDecimal("1000.00"));
         accountsRepository.save(accountTo);
 
-        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom,userTo, new BigDecimal("50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom, userTo, new BigDecimal("50.00"));
         String transferContent = mapper.writeValueAsString(transfer);
 
         mockMvc.perform(post("/transfers")
@@ -132,14 +133,14 @@ public class ApplicationControllerTest {
     public void postTransferNotFromUserReturnsStatusForbidden() throws Exception {
         UserDTO userFrom = new UserDTO(1002L, "jane");
         userRepository.save(userFrom);
-        Accounts accountFrom = new Accounts(2002,1002,new BigDecimal("1000.00"));
+        Accounts accountFrom = new Accounts(2002, 1002, new BigDecimal("1000.00"));
         accountsRepository.save(accountFrom);
         UserDTO userTo = new UserDTO(1001L, "john");
         userRepository.save(userTo);
-        Accounts accountTo = new Accounts(2001,1001,new BigDecimal("1000.00"));
+        Accounts accountTo = new Accounts(2001, 1001, new BigDecimal("1000.00"));
         accountsRepository.save(accountTo);
 
-        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom,userTo, new BigDecimal("50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom, userTo, new BigDecimal("50.00"));
         String transferContent = mapper.writeValueAsString(transfer);
         mockMvc.perform(post("/transfers")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -153,7 +154,7 @@ public class ApplicationControllerTest {
         UserDTO userFrom = new UserDTO(1002L, "jane");
         UserDTO userTo = new UserDTO(1001L, "john");
 
-        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom,userTo, new BigDecimal("-50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom, userTo, new BigDecimal("-50.00"));
         String transferContent = mapper.writeValueAsString(transfer);
         mockMvc.perform(post("/transfers")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -167,10 +168,10 @@ public class ApplicationControllerTest {
 
         String transferContent = "{\"transferId\":3001,\"transferType\":null,\"transferStatus\":\"Approved\",\"userFrom\":{\"id\":1002,\"username\":\"jane\"},\"userTo\":{\"id\":1001,\"username\":\"john\"},\"amount\":50.00}";
 
-            mockMvc.perform(post("/transfers")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(transferContent))
-                    .andExpect(status().isBadRequest());
+        mockMvc.perform(post("/transfers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(transferContent))
+                .andExpect(status().isBadRequest());
     }
 
     @WithMockUser(username = "jane")
@@ -191,7 +192,7 @@ public class ApplicationControllerTest {
         UserDTO userFrom = null;
         UserDTO userTo = new UserDTO(1001L, "john");
 
-        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom,userTo, new BigDecimal("-50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom, userTo, new BigDecimal("-50.00"));
         String transferContent = mapper.writeValueAsString(transfer);
         mockMvc.perform(post("/transfers")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -205,7 +206,7 @@ public class ApplicationControllerTest {
         UserDTO userTo = null;
         UserDTO userFrom = new UserDTO(1001L, "john");
 
-        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom,userTo, new BigDecimal("-50.00"));
+        Transfer transfer = new Transfer(3001L, "Send", "Approved", userFrom, userTo, new BigDecimal("-50.00"));
         String transferContent = mapper.writeValueAsString(transfer);
         mockMvc.perform(post("/transfers")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -219,16 +220,16 @@ public class ApplicationControllerTest {
 
         UserDTO user1 = new UserDTO(1001L, "john");
         userRepository.save(user1);
-        Accounts account1 = new Accounts(2001,1001,new BigDecimal("1000.00"));
+        Accounts account1 = new Accounts(2001, 1001, new BigDecimal("1000.00"));
         accountsRepository.save(account1);
 
-        Transfers transferFrom = new Transfers(3001L,2001L,2002L,new BigDecimal("50.00"));
+        Transfers transferFrom = new Transfers(3001L, 2001L, 2002L, new BigDecimal("50.00"));
         transfersRepository.save(transferFrom);
 
-        Transfers transferTo = new Transfers(3002L,2002L,2001L,new BigDecimal("50.00"));
+        Transfers transferTo = new Transfers(3002L, 2002L, 2001L, new BigDecimal("50.00"));
         transfersRepository.save(transferTo);
 
-        Transfers transferNotIncludingTestUser = new Transfers(3003L,2002L,2003L,new BigDecimal("50.00"));
+        Transfers transferNotIncludingTestUser = new Transfers(3003L, 2002L, 2003L, new BigDecimal("50.00"));
         transfersRepository.save(transferNotIncludingTestUser);
 
         mockMvc.perform(get("/transfers"))
